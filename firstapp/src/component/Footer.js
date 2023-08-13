681d15a7418a8736cbe7f9994514afe405c9ce33
LAPTOP-KLF3DVEL\santa <santanupaikaray1996@gmail.com>
2023-08-13 15:50:46 +0530

    add all files

---
+++ firstapp/src/component/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+ 
+const Footer = () => {
+    return(
+        <footer>
+        <hr/>
+            <center>
+                <h1>Developer Fun</h1>
+            </center>
+            </footer>
+        
+    )
+}
+export default Footer;
---
+++ firstapp/src/component/Home.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+        <h1>My First React App</h1>
+        <h2>Test</h2>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home;
